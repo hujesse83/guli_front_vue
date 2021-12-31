f66cb402139c443af768b6a5cf08280f85a05d9a
hujesse83 <jesse.hu@liveramp.com>
2021-12-31 15:53:31 +0800

    teacher crud

---
+++ src/api/edu/teacher.js
@@ -0,0 +1,37 @@
+import request from "@/utils/request";
+const TEACHER_SERVICE = "/eduservice/teacher";
+export default {
+  getTeacherListPage(current, limit, teacherQuery) {
+    return request({
+      url: TEACHER_SERVICE + `/pageTeacherCondition/${current}/${limit}`,
+      method: "post",
+      data: teacherQuery
+    });
+  },
+  deleteTeacherId(id) {
+    return request({
+      url: TEACHER_SERVICE + `/delete/${id}`,
+      method: "delete"
+    });
+  },
+  addTeacher(teacher) {
+    return request({
+      url: TEACHER_SERVICE + "/addTeacher",
+      method: "post",
+      data: teacher
+    });
+  },
+  getTeacherInfo(id) {
+    return request({
+      url: TEACHER_SERVICE + `/getTeacher/${id}`,
+      method: "get"
+    });
+  },
+  updateTeacherInfo(teacher) {
+    return request({
+      url: TEACHER_SERVICE + `/updateTeacher`,
+      method: "post",
+      data: teacher
+    });
+  }
+};
